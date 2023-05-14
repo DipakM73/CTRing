d592ed7b2d4ee49ab3db6dc4fff8dcb75a389f08
Robert Schneider <robert_schneider@uqar.ca>
2023-05-14 17:23:51 -0400

    Get pith to bark profile

---
+++ R/extractProfile.R
@@ -0,0 +1,26 @@
+#' Get profile between two points of the CTScan image matrix
+#'
+#' @param im Density matrix
+#' @param x Values of X for points 1 and 2 (x1, x2)
+#' @param y Values of y for points 1 and 2 (y1, y2)
+#' @param r Profile width
+#' @param k Rolling window width, interger
+#' @param threshold Threshold value between maximum and minimum density to establish change of ring
+#'
+#' @return
+#'
+#' @export
+#' @import xRing
+#'
+#' @examples
+extractProfile <- function(im,
+                           imHeader,
+                           beginPath,
+                           endPath,
+                           r = 10,
+                           k = 2,
+                           threshold = 0.01) {
+  x <- c(beginPath[1], endPath[1])
+  y <- c(beginPath[2], endPath[2])
+  rings <- ringLimits(im, x, y, r, k, threshold, imHeader)
+}
--- R/pith_helpers.R
+++ R/pith_helpers.R
@@ -102,72 +102,6 @@ lst2arr <- function(lst) {
 pow <- function(x, y)
   x ^ y
 
-#profile of the tree rings between two points (X and Y) using rotated rectangular
-get_profile <- function(im,
-                        x,
-                        y,
-                        r = 10,
-                        k = 5,
-                        threshold = 100) {
-  xDelta <- x[2] - x[1]
-
-  yDelta <- y[2] - y[1]
-
-  dist <- sqrt(pow(xDelta, 2) + pow(yDelta, 2) * 1.0)
-  profileWidth <- 2 * r + 1
-  angle1 <- atan2(-yDelta,-xDelta) * 180.0 / pi
-  ang <- (angle1 + c(1, -1) * 90) * pi / 180
-
-  xVertice <- c(round(x[1] + r * cos(ang[1])),
-                round(x[1] + r * cos(ang[2])))
-  yVertice <- c(round(y[1] + r * sin(ang[1])),
-                round(y[1] + r * sin(ang[2])))
-
-  xLenIncr <- xDelta / (dist - 1)
-  yLenIncr <- yDelta / (dist - 1)
-  xWidthIncr <- (1.0 * xVertice[2] - xVertice[1]) / profileWidth
-  yWidthIncr <- (1.0 * yVertice[2] - yVertice[1]) / profileWidth
-
-  xyLengthDelta_x <-  xyLengthDelta_y <- rep(0, dist)
-  xyWidthDelta_x <- xyWidthDelta_y <- rep(0, profileWidth)
-  xyWidthDelta_x[1] <-  (xVertice[2] - xVertice[1])
-  xyWidthDelta_y[1] <-  (yVertice[2] - yVertice[1])
-
-  i <- 2
-  while (i < dist) {
-    xyLengthDelta_x[i] <- xyLengthDelta_x[i - 1] + xLenIncr
-    xyLengthDelta_y[i] <- xyLengthDelta_y[i - 1] + yLenIncr
-    i <- i + 1
-  }
-
-  i <- 2
-  while (i < profileWidth) {
-    xyWidthDelta_x[i] <- xyWidthDelta_x[i - 1] - xWidthIncr
-    xyWidthDelta_y[i] <- xyWidthDelta_y[i - 1] - yWidthIncr
-    i <- i + 1
-  }
-  xx <- cI(xVertice[1] + xyLengthDelta_x)
-  yy <- cI(yVertice[1] + xyLengthDelta_y)
-  xx_delta <- cI(xyWidthDelta_x)
-  yy_delta <- cI(xyWidthDelta_y)
-  r <- 1
-  out <- matrix(NA, dist, profileWidth)
-
-  while (r < dist) {
-    c <- 1
-    while (c <= profileWidth) {
-      out[r, c] <- im[xx[r] + xx_delta[c], yy[r] + yy_delta[c]]
-      c <- c + 1
-    }
-    r <- r + 1
-  }
-
-  # Tree-ring limits using border function derived from xRing package
-  ring_limits <- border(x = rowMeans(out), k, threshold)
-  xy <-
-    data.frame(x = xx + xx_delta[c / 2], y = yy + yy_delta[c / 2])
-  xy[ring_limits, ]
-}
 
 # function to define segments to find tree-ring borders
 get_segments_up <- function(im,
@@ -270,4 +204,105 @@ intersect_2lines <- function(l1, l2) {
   x <- (l2[1] - l1[1]) / (l1[2] - l2[2])
   y <- l1[1] + l1[2] * x
   c(x, y)
+}
+
+#profile of the tree rings between two points (X and Y) using rotated rectangular
+ringLimits <- function(im,
+                       x,
+                       y,
+                       r = 10,
+                       k = 5,
+                       threshold = 0.1,
+                       imHeader = list()
+) {
+  xDelta <- x[2] - x[1]
+
+  yDelta <- y[2] - y[1]
+
+  dist <- sqrt(pow(xDelta, 2) + pow(yDelta, 2) * 1.0)
+  profileWidth <- 2 * r + 1
+  angle1 <- atan2(-yDelta,-xDelta) * 180.0 / pi
+  ang <- (angle1 + c(1, -1) * 90) * pi / 180
+
+  xVertice <- c(round(x[1] + r * cos(ang[1])),
+                round(x[1] + r * cos(ang[2])))
+  yVertice <- c(round(y[1] + r * sin(ang[1])),
+                round(y[1] + r * sin(ang[2])))
+
+  xLenIncr <- xDelta / (dist - 1)
+  yLenIncr <- yDelta / (dist - 1)
+  xWidthIncr <- (1.0 * xVertice[2] - xVertice[1]) / profileWidth
+  yWidthIncr <- (1.0 * yVertice[2] - yVertice[1]) / profileWidth
+
+  xyLengthDelta_x <-  xyLengthDelta_y <- rep(0, dist)
+  xyWidthDelta_x <- xyWidthDelta_y <- rep(0, profileWidth)
+  xyWidthDelta_x[1] <-  (xVertice[2] - xVertice[1])
+  xyWidthDelta_y[1] <-  (yVertice[2] - yVertice[1])
+
+  i <- 2
+  while (i < dist) {
+    xyLengthDelta_x[i] <- xyLengthDelta_x[i - 1] + xLenIncr
+    xyLengthDelta_y[i] <- xyLengthDelta_y[i - 1] + yLenIncr
+    i <- i + 1
+  }
+
+  i <- 2
+  while (i < profileWidth) {
+    xyWidthDelta_x[i] <- xyWidthDelta_x[i - 1] - xWidthIncr
+    xyWidthDelta_y[i] <- xyWidthDelta_y[i - 1] - yWidthIncr
+    i <- i + 1
+  }
+  xx <- cI(xVertice[1] + xyLengthDelta_x)
+  yy <- cI(yVertice[1] + xyLengthDelta_y)
+  xx_delta <- cI(xyWidthDelta_x)
+  yy_delta <- cI(xyWidthDelta_y)
+  r <- 1
+  out <- matrix(NA, dist, profileWidth)
+
+  while (r < dist) {
+    c <- 1
+    while (c <= profileWidth) {
+      out[r, c] <- im[xx[r] + xx_delta[c], yy[r] + yy_delta[c]]
+      c <- c + 1
+    }
+    r <- r + 1
+  }
+
+  # Tree-ring limits using border function derived from xRing package
+  ring_limits <- border(x = rowMeans(out), k, threshold)
+
+  # density profile
+  dens <- rowMeans(out)
+
+  # Calculate distance from pith
+  distFromPith <- c()
+  if (length(imHeader)!=0){
+    xCoord <- xx * imHeader$pixel_size_x
+    yCoord <- yy * imHeader$pixel_size_y
+    distFromPith <- sqrt((xCoord - xCoord[1])^2 + (yCoord - yCoord[1])^2)
+  }
+
+  returnList <- list(ring_limits = ring_limits,
+                     xx = xx,
+                     yy = yy,
+                     xx_delta = xx_delta,
+                     yy_delta = yy_delta,
+                     c = c,
+                     dens = dens,
+                     distFromPith = distFromPith
+                     )
+  return(returnList)
+}
+
+get_profile <- function(im,
+                        x,
+                        y,
+                        r = 10,
+                        k = 5,
+                        threshold = 0.1) {
+
+  temp <- ringLimits(im, x, y, r, k, threshold)
+  xy <-
+    data.frame(x = temp$xx + temp$xx_delta[temp$c / 2], y = temp$yy + temp$yy_delta[temp$c / 2])
+  xy[temp$ring_limits, ]
 }
