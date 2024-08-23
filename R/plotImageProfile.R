55b2e68cfca79f7af0407918813664ef8ba35afc
Robert Schneider <robert_schneider@uqar.ca>
2024-08-23 10:27:11 -0400

    Adressed errors in build.

--- R/plotImageProfile.R
+++ R/plotImageProfile.R
@@ -3,10 +3,10 @@
 #' @param densProfile Density profile
 #' @param im Density matrix
 #'
-#' @return Plot of ring limits on scan image
+#' @return Plot
 #' @export
 #'
-#'
+#' @examples
 #' library(oro.dicom)
 #' file_path <- system.file("extdata", "disk.dcm", package = "CTRing")
 #' dcm <-  readDICOM(file_path)
